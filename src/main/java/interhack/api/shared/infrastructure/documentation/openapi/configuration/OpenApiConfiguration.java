50b0a798afcf1def136062538246e701cbfab955
Diego Defilippi <defsan35@gmail.com>
2024-10-31 10:32:26 -0500

    feat(shared): Add configuration

---
+++ src/main/java/interhack/api/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -0,0 +1,47 @@
+package interhack.api.shared.infrastructure.documentation.openapi.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+    @Bean
+    public OpenAPI learningPlatformOpenApi() {
+        // General Configuration
+        var openApi = new OpenAPI();
+        openApi
+                .info(new Info()
+                        .title("Interhack Api")
+                        .description("Interhack application REST API documentation.")
+                        .version("v1.0.0")
+                        .license(new License().name("HIGN 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Interhack Documentation")
+                        .url("https://github.com/Finanzas-e-Ingenieria-Economica-Group-3/Interhack.Api/"));
+
+        // Add security scheme
+        final String securitySchemeName = "bearerAuth";
+
+
+        openApi.addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+
+
+        return openApi;
+    }
+}
